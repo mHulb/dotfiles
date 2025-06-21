269beb6311efad96a0e325c34ff1accec28dfc14
Jan Bluhm <jan.niklas.bluhm@gmail.com>
2025-06-21 20:44:05 +0200

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,19 @@
 if status is-interactive
-	# Vim-style navigation with Ctrl key
-	bind \ch backward-char
-	bind \cj down-or-search
-	bind \ck up-or-search
-	bind \cl forward-char
-	bind \u00F6 execute
-	bind \cp backward-kill-word
+    # Vim-style navigation with Ctrl key
+    bind \ch backward-char
+    bind \cj down-or-search
+    bind \ck up-or-search
+    bind \cl forward-char
+    bind \u00F6 execute
+    bind \cp backward-kill-word
 
-        # Ctrl + R for command history search
-        bind \cr 'history | fzf --height 40% --reverse --border | read -l result; and commandline $result'
-	# Ctrl + R for command history search
-	bind \cr 'history | fzf --height 40% --reverse --border | read -l result; and commandline $result'
-	 
+    # Ctrl + R for command history search
+    bind \cr 'history | fzf --height 40% --reverse --border | read -l result; and commandline $result'
+    # Ctrl + R for command history search
+    bind \cr 'history | fzf --height 40% --reverse --border | read -l result; and commandline $result'
+
+    # Fuzzy cd
+    function cdf
+        cd (cdd $argv)
+    end
 end
-         
