24148826f37aafca31bba405518967753ca34c7d
Jan Bluhm <jan.niklas.bluhm@gmail.com>
2025-08-03 16:33:16 +0200

    add curl to install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Update package list and install packages (excluding neovim)
 sudo apt update
-sudo apt install -y fish tmux git stow
+sudo apt install -y fish tmux git stow curl
 
 # Install latest neovim from GitHub releases
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
