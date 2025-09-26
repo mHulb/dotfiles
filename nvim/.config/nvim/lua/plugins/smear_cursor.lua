398f8d56c1d11ea0a2df44942a8cb27f09011de6
Jan Bluhm <jan.niklas.bluhm@gmail.com>
2025-09-26 15:32:49 +0200

    remove smear cursor

--- nvim/.config/nvim/lua/plugins/smear_cursor.lua
+++ nvim/.config/nvim/lua/plugins/smear_cursor.lua
@@ -1,10 +1,10 @@
-return {
-  "sphamba/smear-cursor.nvim",
-  opts = {
-    stiffness = 0.9,
-    trailing_stiffness = 0.5,
-    distance_stop_animating = 0.5,
-    time_interval = 5,
-    never_draw_over_target = false
-  }
-}
+-- return {
+--   "sphamba/smear-cursor.nvim",
+--   opts = {
+--     stiffness = 0.9,
+--     trailing_stiffness = 0.5,
+--     distance_stop_animating = 0.5,
+--     time_interval = 5,
+--     never_draw_over_target = false,
+--   },
+-- }
